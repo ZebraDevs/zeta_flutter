d156428e2cacb93d0ffc98ed49e1e05789631156
Genoveva Georgieva <genoveva.georgieva@athlonsofia.com>
2023-12-01 17:56:29 +0200

    checkbox

---
+++ example/lib/pages/checkbox_example.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:zeta_flutter/zeta_flutter.dart';
+
+import '../widgets.dart';
+
+class CheckBoxExample extends StatefulWidget {
+  static const String name = 'Checkbox';
+
+  const CheckBoxExample({Key? key}) : super(key: key);
+
+  @override
+  State<CheckBoxExample> createState() => _CheckBoxExampleState();
+}
+
+class _CheckBoxExampleState extends State<CheckBoxExample> {
+  bool? isChecked = true;
+  bool isEnabled = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return ExampleScaffold(
+      name: 'Checkbox',
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ZetaCheckbox(
+                    value: isChecked,
+                    isEnabled: isEnabled,
+                    onChanged: (value) => setState(() => isChecked = value)),
+                ElevatedButton(
+                  child: const Text('Disable'),
+                  onPressed: () => setState(() => isEnabled = !isEnabled),
+                )
+              ],
+            ),
+            Row(children: [const Text('Sharp Checkbox Enabled')]),
+            _getCheckBoxRow(isEnabled: true),
+            Row(children: [const Text('Sharp Checkbox Disabled')]),
+            _getCheckBoxRow(isEnabled: false),
+            Row(children: [const Text('Rounded Checkbox Enabled')]),
+            _getCheckBoxRow(isEnabled: true, isSharp: false),
+            Row(children: [const Text('Rounded Checkbox Disabled')]),
+            _getCheckBoxRow(isEnabled: false, isSharp: false),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Row _getCheckBoxRow({required bool isEnabled, bool isSharp = true}) {
+  return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        ZetaCheckbox(
+            value: true,
+            isEnabled: isEnabled,
+            label: 'Selected',
+            borderType: isSharp ? BorderType.sharp : BorderType.rounded,
+            onChanged: (value) => {}),
+        ZetaCheckbox(
+            value: false,
+            isEnabled: isEnabled,
+            label: 'Indeterminate',
+            borderType: isSharp ? BorderType.sharp : BorderType.rounded,
+            onChanged: (value) => {}),
+        ZetaCheckbox(
+            value: null,
+            borderType: isSharp ? BorderType.sharp : BorderType.rounded,
+            isEnabled: isEnabled,
+            onChanged: (value) => {}),
+      ]);
+}
