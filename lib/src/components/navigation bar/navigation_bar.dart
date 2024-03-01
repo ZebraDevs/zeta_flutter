7cb0242b49901cc337e13f655d9f727ba8ac1454
mikecoomber <mike.coomber@zebra.com>
2024-03-01 15:59:11 +0000

    docs

--- lib/src/components/navigation bar/navigation_bar.dart
+++ lib/src/components/navigation bar/navigation_bar.dart
@@ -1,22 +1,34 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import '../../../zeta_flutter.dart';
 
+/// An item to be used in a [ZetaNavigationBar]
 class ZetaNavigationBarItem {
+  /// Creates a new [ZetaNavigationBarItem]
   const ZetaNavigationBarItem({
     required this.icon,
-    this.label,
+    required this.label,
     this.showBadge = false,
     this.badgeValue,
   });
 
+  /// The icon shown on the item.
   final IconData icon;
+
+  /// The label shown on the item.
   final String? label;
+
+  /// Displays a small [ZetaIndicator] on the item.
   final bool showBadge;
+
+  /// Displays a medium [ZetaIndicator] on the item with the given value.
   final int? badgeValue;
 }
 
+/// Navigation Bars (Bottom navigation) allow movement between primary destinations in an app.
 class ZetaNavigationBar extends StatelessWidget {
+  /// Creates a new [ZetaNavigationBar].
   const ZetaNavigationBar({
     required this.items,
     this.currentIndex,
@@ -30,6 +42,7 @@ class ZetaNavigationBar extends StatelessWidget {
           'The number of items should be between 2 and 6',
         );
 
+  /// Creates a [ZetaNavigationBar] with a divider after the item at the given index.
   const ZetaNavigationBar.divided({
     required List<ZetaNavigationBarItem> items,
     required int? dividerIndex,
@@ -45,6 +58,7 @@ class ZetaNavigationBar extends StatelessWidget {
           key: key,
         );
 
+  /// Creates a [ZetaNavigationBar] and splits the items in half.
   const ZetaNavigationBar.split({
     required List<ZetaNavigationBarItem> items,
     int? currentIndex,
@@ -58,6 +72,7 @@ class ZetaNavigationBar extends StatelessWidget {
           key: key,
         );
 
+  /// Creates a [ZetaNavigationBar] with an action.
   const ZetaNavigationBar.action({
     required List<ZetaNavigationBarItem> items,
     required Widget action,
@@ -72,24 +87,36 @@ class ZetaNavigationBar extends StatelessWidget {
           key: key,
         );
 
+  /// The items displayed on the navigation bar.
   final List<ZetaNavigationBarItem> items;
+
+  /// The index of the currently active item.
   final int? currentIndex;
+
+  /// Called when an item is tapped with the index of the tapped item.
   final void Function(int value)? onTap;
+
+  /// Divides the navigation items in half.
   final bool splitItems;
+
+  /// The index of the item the divider should be displayed after.
   final int? dividerIndex;
+
+  /// The action shown on the navigation bar.
   final Widget? action;
 
   Row _generateNavigationItemRow(List<ZetaNavigationBarItem> items) {
     return Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: items.map((navItem) {
-          final index = items.indexOf(navItem);
-          return _NavigationItem(
-            selected: index == currentIndex,
-            item: navItem,
-            onTap: () => onTap?.call(index),
-          );
-        }).toList());
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: items.map((navItem) {
+        final index = items.indexOf(navItem);
+        return _NavigationItem(
+          selected: index == currentIndex,
+          item: navItem,
+          onTap: () => onTap?.call(index),
+        );
+      }).toList(),
+    );
   }
 
   @override
@@ -145,6 +172,17 @@ class ZetaNavigationBar extends StatelessWidget {
       ),
       child: child,
     );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(IterableProperty<ZetaNavigationBarItem>('items', items))
+      ..add(IntProperty('currentIndex', currentIndex))
+      ..add(ObjectFlagProperty<void Function(int value)?>.has('onTap', onTap))
+      ..add(DiagnosticsProperty<bool>('splitItems', splitItems))
+      ..add(IntProperty('dividerIndex', dividerIndex));
   }
 }
 
@@ -220,5 +258,14 @@ class _NavigationItem extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty<bool>('selected', selected))
+      ..add(DiagnosticsProperty<ZetaNavigationBarItem>('item', item))
+      ..add(ObjectFlagProperty<VoidCallback>.has('onTap', onTap));
   }
 }
