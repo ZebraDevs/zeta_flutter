4dbdeb82054fdf08eec100bd2f067fafc38da9c4
atanasyordanov21 <63714308+atanasyordanov21@users.noreply.github.com>
2024-04-29 18:00:44 +0300

    ZetaScreenHeaderBar (#39)

---
+++ example/lib/pages/components/screen_header_bar_example.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:zeta_example/widgets.dart';
+import 'package:zeta_flutter/zeta_flutter.dart';
+
+class ScreenHeaderBarExample extends StatelessWidget {
+  const ScreenHeaderBarExample({super.key});
+
+  static const String name = 'ScreenHeaderBar';
+
+  @override
+  Widget build(BuildContext context) {
+    return ExampleScaffold(
+      name: ScreenHeaderBarExample.name,
+      child: ZetaScreenHeaderBar(
+        title: Text("Add Subscribers"),
+        actionButtonLabel: 'Done',
+        onActionButtonPressed: () {},
+      ),
+    );
+  }
+}
---
+++ lib/src/components/screen_header_bar/screen_header_bar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import '../../../zeta_flutter.dart';
+
+/// [ZetaScreenHeaderBar]
+class ZetaScreenHeaderBar extends StatelessWidget {
+  /// Constructor for [ZetaScreenHeaderBar].
+  const ZetaScreenHeaderBar({
+    this.title,
+    this.rounded = true,
+    this.actionButtonLabel,
+    this.onActionButtonPressed,
+    super.key,
+  });
+
+  /// The title of [ZetaScreenHeaderBar]. Normally a [Text] widget.
+  final Widget? title;
+
+  /// {@macro zeta-component-rounded}
+  final bool rounded;
+
+  /// The label of the action button.
+  final String? actionButtonLabel;
+
+  /// Called when the action button is pressed.
+  final VoidCallback? onActionButtonPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ZetaAppBar(
+      leading: IconButton(
+        onPressed: () async => Navigator.maybePop(context),
+        icon: Icon(rounded ? ZetaIcons.chevron_left_round : ZetaIcons.chevron_left_sharp),
+      ),
+      title: title,
+      titleSpacing: 0,
+      titleTextStyle: ZetaTextStyles.titleLarge,
+      actions: actionButtonLabel == null
+          ? null
+          : [
+              ZetaButton(
+                label: actionButtonLabel!,
+                onPressed: onActionButtonPressed,
+                borderType: rounded ? ZetaWidgetBorder.rounded : ZetaWidgetBorder.sharp,
+              ),
+            ],
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty<bool>('rounded', rounded))
+      ..add(StringProperty('actionButtonLabel', actionButtonLabel))
+      ..add(ObjectFlagProperty<VoidCallback?>.has('onActionButtonPressed', onActionButtonPressed));
+  }
+}
