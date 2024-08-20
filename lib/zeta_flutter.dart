c2458d0d48b26eb6f74780489c4a9b8e0644e1b4
Luke <lwalton@zebra.com>
2024-08-20 14:49:26 +0100

    chore: Organise assets within repo

--- lib/zeta_flutter.dart
+++ lib/zeta_flutter.dart
@@ -1,7 +1,7 @@
 /// Zebra Design System (Zeta) - Flutter Component Library
 library zeta_flutter;
 
-export 'src/assets/assets.dart';
+export 'generated/generated.dart';
 export 'src/components/components.dart';
 export 'src/theme/theme.dart';
 export 'src/utils/utils.dart';
