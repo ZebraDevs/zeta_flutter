80165d0f685fd92f7c81b5a40d0ad4d7ff4ae4e0
Daniel Eshkeri <de7924@zebra.com>
2024-09-17 16:23:20 +0100

    docs: missed one

--- lib/src/components/list_item/list_item.dart
+++ lib/src/components/list_item/list_item.dart
@@ -54,6 +54,10 @@ class ZetaList extends ZetaStatelessWidget {
 ///
 /// To create list items with a [ZetaSwitch], [ZetaCheckbox], or [ZetaRadio], use the [ZetaListItem.toggle], [ZetaListItem.checkbox] or the [ZetaListItem.radio] named constructors respectively.
 /// {@category Components}
+///
+/// Figma: https://www.figma.com/design/JesXQFLaPJLc1BdBM4sisI/%F0%9F%A6%93-ZDS---Components?node-id=229-17&node-type=canvas&m=dev
+///
+/// Widgetbook: https://zeta-ds.web.app/flutter/widgetbook/index.html#/?path=components/list-items/list-item
 class ZetaListItem extends ZetaStatelessWidget {
   /// Creates a [ZetaListItem].
   const ZetaListItem({
