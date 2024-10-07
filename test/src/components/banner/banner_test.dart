de681a94babecbc3d4ad2b5c3ef63fef5efcdd17
Daniel Eshkeri <de7924@zebra.com>
2024-10-07 17:40:40 +0100

    tests: banner tests
    fix: added styles to banner text
    
    docs: added testing_conventions.mdx to keep track how we are testing in flutter

--- test/src/components/banner/banner_test.dart
+++ test/src/components/banner/banner_test.dart
@@ -6,20 +6,140 @@ import 'package:zeta_flutter/zeta_flutter.dart';
 import '../../../test_utils/test_app.dart';
 import '../../../test_utils/utils.dart';
 
-void main() {
-  testWidgets('ZetaBanner title is correct', (WidgetTester tester) async {
-    final BuildContext context = tester.element(find.byType(TestApp));
-    await tester.pumpWidget(
-      TestApp(
-        home: ZetaBanner(
-          context: context,
-          title: 'Banner Title',
-        ),
-      ),
-    );
+ZetaColorSwatch _backgroundColorFromType(BuildContext context, ZetaBannerStatus type) {
+  final zeta = Zeta.of(context);
 
-    expect(find.text('Banner Title'), findsOneWidget);
+  switch (type) {
+    case ZetaBannerStatus.primary:
+      return zeta.colors.primary;
+    case ZetaBannerStatus.positive:
+      return zeta.colors.surfacePositive;
+    case ZetaBannerStatus.warning:
+      return zeta.colors.orange;
+    case ZetaBannerStatus.negative:
+      return zeta.colors.surfaceNegative;
+  }
+}
+
+void main() {
+  group('ZetaBanner Styling Tests', () {
+    for (final type in ZetaBannerStatus.values) {
+      testWidgets('ZetaBanner title styles are correct for $type', (WidgetTester tester) async {
+        await tester.pumpWidget(
+          TestApp(
+            home: Builder(
+              builder: (context) {
+                return ZetaBanner(
+                  context: context,
+                  title: 'Banner Title',
+                  type: type,
+                );
+              },
+            ),
+          ),
+        );
+        final Finder textFinder = find.text('Banner Title');
+        final Text textWidget = tester.widget(find.byType(Text));
+        expect(
+          textWidget.style,
+          equals(
+            ZetaTextStyles.labelLarge.copyWith(
+              color: Zeta.of(tester.element(textFinder)).colors.textInverse,
+            ),
+          ),
+        );
+      });
+    }
+
+    for (final type in ZetaBannerStatus.values) {
+      testWidgets('ZetaBanner icon color is correct for $type', (WidgetTester tester) async {
+        await tester.pumpWidget(
+          TestApp(
+            home: Builder(
+              builder: (context) {
+                return ZetaBanner(
+                  context: context,
+                  title: 'Banner Title',
+                  leadingIcon: Icons.info,
+                  type: type,
+                );
+              },
+            ),
+          ),
+        );
+        final Finder iconFinder = find.byIcon(Icons.info);
+
+        // Color
+        final Icon iconWidget = tester.widget(iconFinder);
+        expect(iconWidget.color, _backgroundColorFromType(tester.element(iconFinder), type).onColor);
+      });
+    }
   });
+
+  group('ZetaBanner Content Tests', () {
+    testWidgets('ZetaBanner title is correct', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        TestApp(
+          home: Builder(
+            builder: (context) {
+              return ZetaBanner(
+                context: context,
+                title: 'Banner Title',
+              );
+            },
+          ),
+        ),
+      );
+      final Finder textFinder = find.text('Banner Title');
+      expect(textFinder, findsOneWidget);
+    });
+
+    testWidgets('ZetaBanner icon is correct', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        TestApp(
+          home: Builder(
+            builder: (context) {
+              return ZetaBanner(
+                context: context,
+                title: 'Banner Title',
+                leadingIcon: Icons.info,
+              );
+            },
+          ),
+        ),
+      );
+      final Finder iconFinder = find.byIcon(Icons.info);
+      expect(iconFinder, findsOneWidget);
+
+      final Icon iconWidget = tester.widget(iconFinder);
+      expect(iconWidget.icon, equals(Icons.info));
+    });
+  });
+
+  group('ZetaBanner Dimension Tests', () {
+    testWidgets('ZetaBanner icon is the correct size', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        TestApp(
+          home: Builder(
+            builder: (context) {
+              return ZetaBanner(
+                context: context,
+                title: 'Banner Title',
+                leadingIcon: Icons.info,
+              );
+            },
+          ),
+        ),
+      );
+      final Finder iconFinder = find.byIcon(Icons.info);
+
+      final Icon iconWidget = tester.widget(iconFinder);
+
+      expect(iconWidget.size, Zeta.of(tester.element(iconFinder)).spacing.xl_2);
+    });
+  });
+
+  group('ZetaBanner Interaction Tests', () {});
 
   testWidgets('debugFillProperties works correctly', (WidgetTester tester) async {
     final diagnostics = DiagnosticPropertiesBuilder();
