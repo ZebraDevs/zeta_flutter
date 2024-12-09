1bd8bfa0a8a6fda8782860779fa02e1d195f67ea
Luke Walton <thelukewalton@users.noreply.github.com>
2024-12-09 18:00:31 +0000

    deps(automated): Update icons (#219)
    * deps(automated): Update icons 05 Dec 2024, 10:18:32
    
    * chore(automated): Lint commit and format
    
    ---------
    
    Co-authored-by: zeta-icons-bot <zeta-icons-bot@github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- lib/generated/icons/icons.g.dart
+++ lib/generated/icons/icons.g.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 
 /// Current version of Zeta Icons
 // x-release-please-start-version
-const zetaIconsVersion = '0.9.0';
+const zetaIconsVersion = '0.9.1';
 // x-release-please-end
 
 /// Zeta Icons. Full list of icons can be found at [Zeta Icons](https://design.zebra.com/icons/).
