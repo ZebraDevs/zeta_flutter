91d25768fbc715d1dc86f87c440c30c94a41242b
thelukewalton <lwalton@zebra.com>
2025-07-24 16:31:42 +0100

    wav only support working

---
+++ example/lib/pages/components/voice_memo_example.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:zeta_example/widgets.dart';
+import 'package:zeta_flutter/zeta_flutter.dart';
+
+class VoiceMemoExample extends StatefulWidget {
+  const VoiceMemoExample({super.key});
+
+  static const String name = 'VoiceMemo';
+
+  @override
+  State<VoiceMemoExample> createState() => _VoiceMemoExampleState();
+}
+
+class _VoiceMemoExampleState extends State<VoiceMemoExample> {
+  @override
+  Widget build(BuildContext context) {
+    return ExampleScaffold(
+      name: VoiceMemoExample.name,
+      paddingAll: 0,
+      gap: 4,
+      children: [
+        const ZetaAudioVisualizer(
+          url: 'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_1MB_WAV.wav',
+        ),
+        const ZetaAudioVisualizer(
+          assetPath: 'assets/notification.wav',
+        ),
+      ].gap(20),
+    );
+  }
+}
--- packages/zeta_flutter/lib/src/components/voice_memo/audio_helpers.dart
+++ packages/zeta_flutter/lib/src/components/voice_memo/audio_helpers.dart
@@ -1,93 +1,116 @@
-// ignore_for_file: avoid_catching_errors, public_member_api_docs
-
 import 'dart:async';
 
-import 'package:file/file.dart';
 import 'package:file/local.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
-import 'package:path_provider/path_provider.dart';
 
-Uri _sanitizeURLForWeb(String fileName) {
-  final tryAbsolute = Uri.tryParse(fileName);
-  if (tryAbsolute?.isAbsolute ?? false) {
-    return tryAbsolute!;
+/// Extracts WAV amplitudes from a file URI, normalizing them for visualization.
+///
+/// This function is very basic, and does not handle all WAV formats, only PCM.
+///
+/// The extracted amplitudes are normalized to a range of 0.0 to 1.0, where 0.5 represents the average amplitude.
+/// These values should not be considered accurate for audio playback, but rather for visual representation in a waveform.
+/// Extracts WAV amplitudes from a file URI, normalizing them for visualization.
+///
+/// This function is very basic, and does not handle all WAV formats, only PCM.
+///
+/// The extracted amplitudes are normalized to a range of 0.0 to 1.0, where 0.5 represents the average amplitude.
+/// These values should not be considered accurate for audio playback, but rather for visual representation in a waveform.
+///
+/// If the presented file is not a WAV file, it will return null.
+Future<List<double>?> extractWavAmplitudes(Uri fileUri, int linesNeeded) async {
+  try {
+    final file = const LocalFileSystem().file(fileUri.toFilePath());
+    final bytes = await file.readAsBytes();
+
+    return _parseWav(bytes, linesNeeded);
+  } catch (e, stackTrace) {
+    debugPrint('Error extracting WAV amplitudes: $e');
+    debugPrint('Stack trace: $stackTrace');
+    return null;
   }
-
-  // Relative Asset path
-  // URL-encode twice, see:
-  // https://github.com/flutter/engine/blob/2d39e672c95efc6c539d9b48b2cccc65df290cc4/lib/web_ui/lib/ui_web/src/ui_web/asset_manager.dart#L61
-  // Parsing an already encoded string to an Uri does not encode it a second
-  // time, so we have to do it manually:
-  final encoded = UriCoder.encodeOnce(fileName);
-  return Uri.parse(Uri.encodeFull('assets/assets/$encoded'));
 }
 
-Future<ByteData> loadAsset(String path) => rootBundle.load(path);
+List<double> _decodePCM(List<int> audioBytes, int bitsPerSample, int numChannels) {
+  final bytesPerSample = bitsPerSample ~/ 8;
+  final totalSamples = audioBytes.length ~/ (bytesPerSample * numChannels);
+  final samples = List<double>.filled(totalSamples, 0);
 
-extension UriCoder on Uri {
-  static String encodeOnce(String uri) {
-    try {
-      // If decoded differs, the uri was already encoded.
-      final decodedUri = Uri.decodeFull(uri);
-      if (decodedUri != uri) {
-        return uri;
+  for (int i = 0; i < totalSamples; i++) {
+    final sampleStart = i * bytesPerSample * numChannels;
+    double sampleValue = 0;
+
+    for (int channel = 0; channel < numChannels; channel++) {
+      final channelStart = sampleStart + channel * bytesPerSample;
+      int sample = 0;
+
+      for (int byteIndex = 0; byteIndex < bytesPerSample; byteIndex++) {
+        sample |= audioBytes[channelStart + byteIndex] << (byteIndex * 8);
       }
-    } on ArgumentError catch (_) {}
-    return Uri.encodeFull(uri);
+
+      sample = sample.toSigned(bitsPerSample);
+      sampleValue += sample.toDouble();
+    }
+
+    samples[i] = sampleValue / numChannels; // Average across channels
   }
+
+  return samples;
 }
 
-Future<Uri> fetchToMemory(String fileName) async {
-  if (kIsWeb) {
-    final uri = _sanitizeURLForWeb(fileName);
-    // We rely on browser caching here. Once the browser downloads this file,
-    // the native side implementation should be able to access it from cache.
-    await http.get(uri);
-    return uri;
-  }
-  const FileSystem fileSystem = LocalFileSystem();
-  final tempDir = await getTempDir();
-  final filePath = '$tempDir$fileName';
-  final file = fileSystem.file(filePath);
+Future<List<double>> _parseWav(Uint8List bytes, int linesNeeded) async {
+  // Get these values from the WAV header
+  final audioFormat = bytes[20] | (bytes[21] << 8);
+  final numChannels = bytes[22] | (bytes[23] << 8);
+  final bitsPerSample = bytes[34] | (bytes[35] << 8);
 
-  // Check if the file already exists
-  if (await file.exists()) {
-    return file.uri;
+  if (audioFormat != 1) {
+    throw UnsupportedError('Unsupported WAV format: Only PCM is supported');
   }
 
-  // Read local asset from rootBundle and store it
-  final byteData = await loadAsset(fileName);
-  await file.create(recursive: true);
-  await file.writeAsBytes(byteData.buffer.asUint8List());
+  final dataOffset = _findDataChunkOffset(bytes);
+  final audioBytes = bytes.sublist(dataOffset);
+  final bytesPerSample = bitsPerSample ~/ 8;
+  final totalSamples = audioBytes.length ~/ (bytesPerSample * numChannels);
 
-  return file.uri;
+  if (linesNeeded <= 0 || linesNeeded > totalSamples) {
+    throw RangeError('Invalid linesNeeded value: $linesNeeded');
+  }
+
+  final audioSamples = _decodePCM(audioBytes, bitsPerSample, numChannels);
+  return _groupAndNormalizeSamples(audioSamples, linesNeeded);
 }
 
-Future<Uri> downloadAudioFileToLocal(String url) async {
-  const FileSystem fileSystem = LocalFileSystem();
-  final tempDir = await getTempDir();
-
-  final encodedUrl = UriCoder.encodeOnce(url);
-  final filePath = '$tempDir/$encodedUrl';
-  final file = fileSystem.file(filePath);
-
-  // Check if the file already exists
-  if (await file.exists()) {
-    return file.uri;
+int _findDataChunkOffset(Uint8List bytes) {
+  for (int i = 36; i < bytes.length - 8; i += 2) {
+    if (bytes[i] == 0x64 && bytes[i + 1] == 0x61 && bytes[i + 2] == 0x74 && bytes[i + 3] == 0x61) {
+      return i + 8;
+    }
   }
-
-  // Download the file if it doesn't exist
-  final response = await http.get(Uri.parse(url));
-  if (response.statusCode != 200) {
-    throw Exception('Failed to download audio file');
-  }
-
-  await file.create(recursive: true);
-  await file.writeAsBytes(response.bodyBytes);
-
-  return file.uri;
+  return 44; // Default offset for PCM WAV files
 }
 
-Future<String> getTempDir() async => (await getTemporaryDirectory()).path;
+List<double> _groupAndNormalizeSamples(List<double> samples, int groupsCount) {
+  final groupSize = samples.length ~/ groupsCount;
+  final groups = List<double>.filled(groupsCount, 0);
+
+  for (int i = 0; i < samples.length; i++) {
+    final groupIndex = i ~/ groupSize;
+    if (groupIndex < groupsCount) {
+      groups[groupIndex] += samples[i];
+    }
+  }
+
+  for (int i = 0; i < groupsCount; i++) {
+    groups[i] /= groupSize;
+  }
+
+  final minAmplitude = groups.reduce((a, b) => a < b ? a : b);
+  final maxAmplitude = groups.reduce((a, b) => a > b ? a : b);
+
+  if (maxAmplitude == minAmplitude) {
+    return List<double>.filled(groupsCount, 0.5);
+  }
+
+  final range = maxAmplitude - minAmplitude;
+  return groups.map((e) => (e - minAmplitude) / range).toList();
+}
