2c310f9f97083a9c6e2c36f7a4fe97c9ffbfe4b6
thelukewalton <lwalton@zebra.com>
2025-07-25 14:53:56 +0100

    add color overrides for voice memo in chat bubble

--- example/lib/pages/components/voice_memo_example.dart
+++ example/lib/pages/components/voice_memo_example.dart
@@ -22,12 +22,43 @@ class _VoiceMemoExampleState extends State<VoiceMemoExample> {
         const ZetaAudioVisualizer(
           url: 'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_10MB_WAV.wav',
         ),
-        // const ZetaAudioVisualizer(
-        //   url: 'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_1MB_WAV.wav',
-        // ),
-        // const ZetaAudioVisualizer(
-        //   assetPath: 'assets/notification.wav',
-        // ),
+        const ZetaAudioVisualizer(
+          url: 'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_1MB_WAV.wav',
+        ),
+        const ZetaAudioVisualizer(
+          assetPath: 'assets/notification.wav',
+        ),
+        Container(
+          alignment: Alignment.centerRight,
+          width: 345,
+          decoration: BoxDecoration(
+            color: Zeta.of(context).colors.mainPrimary,
+            borderRadius: BorderRadius.all(Zeta.of(context).radius.rounded),
+          ),
+          child: Column(
+            children: [
+              ZetaAudioVisualizer(
+                assetPath: 'assets/notification.wav',
+                backgroundColor: Zeta.of(context).colors.mainPrimary,
+                foregroundColor: Zeta.of(context).colors.mainInverse,
+                tertiaryColor: Zeta.of(context).colors.mainDisabled,
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: Zeta.of(context).spacing.medium,
+                  right: Zeta.of(context).spacing.medium,
+                  bottom: Zeta.of(context).spacing.medium,
+                ),
+                child: Text(
+                  "Hey there! Just wanted to remind you about our meeting tomorrow at 10 AM. Let's catch up then!",
+                  style: Zeta.of(context).textStyles.bodyXSmall.copyWith(
+                        color: Zeta.of(context).colors.mainInverse,
+                      ),
+                ),
+              ),
+            ],
+          ),
+        )
       ].gap(20),
     );
   }
