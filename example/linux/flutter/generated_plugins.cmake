a3b78672b79609484435a8eeb90fbbc499f5221b
thelukewalton <lwalton@zebra.com>
2025-07-30 16:50:46 +0100

    basic recording

--- example/linux/flutter/generated_plugins.cmake
+++ example/linux/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_linux
+  record_linux
   url_launcher_linux
 )
 
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_windows
+  record_windows
   url_launcher_windows
 )
 
