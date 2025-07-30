dfcd88bb58b5c9ac4d87afa6d56af81784201a68
thelukewalton <lwalton@zebra.com>
2025-07-30 10:38:03 +0100

    reduce number of redraws

--- example/lib/pages/components/voice_memo_example.dart
+++ example/lib/pages/components/voice_memo_example.dart
@@ -16,56 +16,55 @@ class _VoiceMemoExampleState extends State<VoiceMemoExample> {
   Widget build(BuildContext context) {
     return ExampleScaffold(
       name: VoiceMemoExample.name,
-      paddingAll: 0,
       gap: 4,
       children: [
         const ZetaAudioVisualizer(
           url: 'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_10MB_WAV.wav',
-        ),
-        const ZetaAudioVisualizer(
-          url: 'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_1MB_WAV.wav',
-        ),
-        const ZetaAudioVisualizer(
-          assetPath: 'assets/notification.wav',
-        ),
-        Container(
-          alignment: Alignment.centerRight,
-          width: 345,
-          decoration: BoxDecoration(
-            color: Zeta.of(context).colors.mainPrimary,
-            borderRadius: BorderRadius.all(Zeta.of(context).radius.rounded),
-          ),
-          child: Column(
-            children: [
-              ZetaAudioVisualizer(
-                assetPath: 'assets/notification.wav',
-                backgroundColor: Zeta.of(context).colors.mainPrimary,
-                foregroundColor: Zeta.of(context).colors.mainInverse,
-                tertiaryColor: Zeta.of(context).colors.mainDisabled,
-                playButtonColor: Zeta.of(context).colors.mainInverse,
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                  left: Zeta.of(context).spacing.medium,
-                  right: Zeta.of(context).spacing.medium,
-                  bottom: Zeta.of(context).spacing.medium,
-                ),
-                child: Text(
-                  "Hey there! Just wanted to remind you about our meeting tomorrow at 10 AM. Let's catch up then!",
-                  style: Zeta.of(context).textStyles.bodyXSmall.copyWith(
-                        color: Zeta.of(context).colors.mainInverse,
-                      ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        Divider(),
-        SizedBox(
-          width: 375,
-          child: ZetaVoiceMemo(),
-        ),
-        Divider(),
+        ).paddingAll(8),
+        // const ZetaAudioVisualizer(
+        //   url: 'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_1MB_WAV.wav',
+        // ),
+        // const ZetaAudioVisualizer(
+        //   assetPath: 'assets/notification.wav',
+        // ),
+        // Container(
+        //   alignment: Alignment.centerRight,
+        //   width: 345,
+        //   decoration: BoxDecoration(
+        //     color: Zeta.of(context).colors.mainPrimary,
+        //     borderRadius: BorderRadius.all(Zeta.of(context).radius.rounded),
+        //   ),
+        //   child: Column(
+        //     children: [
+        //       ZetaAudioVisualizer(
+        //         assetPath: 'assets/notification.wav',
+        //         backgroundColor: Zeta.of(context).colors.mainPrimary,
+        //         foregroundColor: Zeta.of(context).colors.mainInverse,
+        //         tertiaryColor: Zeta.of(context).colors.mainDisabled,
+        //         playButtonColor: Zeta.of(context).colors.mainInverse,
+        //       ),
+        //       Padding(
+        //         padding: EdgeInsets.only(
+        //           left: Zeta.of(context).spacing.medium,
+        //           right: Zeta.of(context).spacing.medium,
+        //           bottom: Zeta.of(context).spacing.medium,
+        //         ),
+        //         child: Text(
+        //           "Hey there! Just wanted to remind you about our meeting tomorrow at 10 AM. Let's catch up then!",
+        //           style: Zeta.of(context).textStyles.bodyXSmall.copyWith(
+        //                 color: Zeta.of(context).colors.mainInverse,
+        //               ),
+        //         ),
+        //       ),
+        //     ],
+        //   ),
+        // ),
+        // Divider(),
+        // SizedBox(
+        //   width: 375,
+        //   child: ZetaVoiceMemo(),
+        // ),
+        // Divider(),
       ].gap(20),
     );
   }
