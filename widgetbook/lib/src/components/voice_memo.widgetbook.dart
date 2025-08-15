86dd15f848d4fb4cb64a9ed24c1aacec4c916c86
thelukewalton <lwalton@zebra.com>
2025-08-15 14:09:56 +0100

    docs: widgetbook

--- widgetbook/lib/src/components/voice_memo.widgetbook.dart
+++ widgetbook/lib/src/components/voice_memo.widgetbook.dart
@@ -13,7 +13,32 @@ const String path = '$componentsPath/Voice Memo';
   designLink:
       'https://www.figma.com/design/JesXQFLaPJLc1BdBM4sisI/%F0%9F%A6%93-ZDS---Components?node-id=38832-2389&t=MGPsY57Ld4bcpPeQ-4',
 )
-Widget voiceMemo(BuildContext context) => ZetaVoiceMemo();
+Widget voiceMemo(BuildContext context) => ZetaVoiceMemo(
+      canRecord: context.knobs.boolean(label: "Can record", initialValue: true),
+      maxLimitLabel: context.knobs.string(
+        label: "Max limit label",
+        initialValue: "Recording message {timer} seconds left...",
+      ),
+      maxRecordingDuration: Duration(
+        seconds: context.knobs.int.slider(label: "Max recording duration", initialValue: 10).toInt(),
+      ),
+      playingLabel: context.knobs.string(label: "Playing label", initialValue: "Playing..."),
+      warningDuration: Duration(
+        seconds: context.knobs.int.slider(label: "Warning duration", initialValue: 5).toInt(),
+      ),
+      sendMessageLabel: context.knobs.string(
+        label: "Send message label",
+        initialValue: "Send message?",
+      ),
+      recordingLabel: context.knobs.string(
+        label: "Recording label",
+        initialValue: "Recording...",
+      ),
+      recordingNotAllowedLabel: context.knobs.string(
+        label: "Recording not allowed label",
+        initialValue: "Recording not allowed.",
+      ),
+    );
 
 @widgetbook.UseCase(
   name: 'Audio Visualizer',
@@ -29,4 +54,5 @@ Widget audioVisualizer(BuildContext context) => ZetaAudioVisualizer(
       playButtonColor: context.knobs.colorOrNull(label: "Play button color"),
       rounded: context.knobs.boolean(label: "Rounded", initialValue: true),
       tertiaryColor: context.knobs.colorOrNull(label: "Tertiary color"),
+      errorMessage: context.knobs.string(label: "Error message", initialValue: "Audio cannot be played"),
     );
