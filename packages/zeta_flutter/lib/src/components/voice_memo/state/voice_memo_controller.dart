541c786f893e9bbec477f246261e368d1fb8a9da
thelukewalton <lwalton@zebra.com>
2025-08-15 13:03:01 +0100

    fix: Audio recording on web

--- packages/zeta_flutter/lib/src/components/voice_memo/state/voice_memo_controller.dart
+++ packages/zeta_flutter/lib/src/components/voice_memo/state/voice_memo_controller.dart
@@ -42,8 +42,8 @@ class VoiceMemoController {
     );
   }
 
-  void restartRecording(VoidCallback setWarning) {
-    recordingManager.resetRecording();
+  void restartRecording(VoidCallback setWarning, AudioPlaybackManager playbackManager) {
+    recordingManager.resetRecording(playbackManager);
     setWarning();
   }
 }
